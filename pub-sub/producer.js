803357e415035aafbcd4a0f9840e617b3e81d4b3
Serhat Ayata <srhtayata@gmail.com>
2022-05-25 15:43:19 +0300

    pub-sub started

---
+++ pub-sub/producer.js
@@ -0,0 +1,41 @@
+const { Kafka } = require("kafkajs");
+
+const log_data = require("./system_logs.json")
+
+createProducer();
+
+async function createProducer() {
+  try {
+    
+    const kafka = new Kafka({
+      clientId: "kafka_log_store_client",
+      brokers: ["192.168.56.1:9092"]
+    });
+
+    const producer = kafka.producer();
+    console.log("Producer'a bağlanılıyor...");
+    await producer.connect();
+    console.log("Producer'a bağlantı başarılı");
+
+    let messages = log_data.map(item => {
+      return {
+        value:JSON.stringify(item),
+        partition:item.type == "system" ? 0 : 1
+      }
+    });
+
+    const message_result = await producer.send({
+      topic: "LogStoreTopic",
+      messages: messages
+    })
+
+    console.log("Gonderim basarilidir", JSON.stringify(message_result));
+    await producer.disconnect();
+
+
+  } catch (error) {
+    console.log("Bir Hata Oluştu", error);
+  } finally {
+    process.exit(0);
+  }
+}
